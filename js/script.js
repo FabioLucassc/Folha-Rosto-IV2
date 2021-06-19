2c2f4cafa029f68afdb70eeb584826e074960f49
Fabio Lucas <62105051+FabioLucassc@users.noreply.github.com>
2021-06-18 23:46:56 -0300

    up

---
+++ js/script.js
@@ -0,0 +1,98 @@
+alert("Olá, bem-vindo(a) a minha página, espero que goste!\n\nCaso deseje mais informações os meios de contato se encontram no canto inferior direito da página.");
+
+
+//  --------------------------------- Titulo Paragrafo ------------------------------ //
+
+function mouseOverTitulo() {
+
+  document.getElementById("titulo").style.textShadow = "1px 2px 10px black		";
+
+}
+function mouseOutTitulo() {
+
+  document.getElementById("titulo").style.textShadow = "0px 0px 0px #0000";
+}
+
+//  --------------------------------- Titulo Paragrafo ------------------------------ //
+
+
+
+//  --------------------------------- Subtitulo Paragrafo ------------------------------ //
+
+function mouseOverSubtitulo() {
+
+  document.getElementById("subtituloDesc").style.textShadow = "1px 2px 10px black		";
+
+}
+function mouseOutSubtitulo() {
+
+  document.getElementById("subtituloDesc").style.textShadow = "0px 0px 0px #0000";
+}
+
+//  --------------------------------- Subtitulo Paragrafo ------------------------------ //
+
+
+
+//  --------------------------------- Primeiro Paragrafo ------------------------------ //
+
+function mouseOverIni() {
+
+  document.getElementById("primeiroParagrafo").style.textShadow = "1px 2px 5px #C0C0C0		";
+
+}
+function mouseOutIni() {
+
+  document.getElementById("primeiroParagrafo").style.textShadow = "0px 0px 0px #0000";
+}
+
+//  --------------------------------- Primeiro Paragrafo ------------------------------ //
+
+
+
+
+//  --------------------------------- Segundo Paragrafo ------------------------------ //
+
+function mouseOverSeg() {
+
+  document.getElementById("segundoParagrafo").style.textShadow = "1px 2px 5px #C0C0C0		";
+
+}
+function mouseOutSeg() {
+
+  document.getElementById("segundoParagrafo").style.textShadow = "0px 0px 0px #0000";
+}
+
+//  --------------------------------- Segundo Paragrafo ------------------------------ //
+
+
+
+
+//  --------------------------------- Terceiro Paragrafo ------------------------------ //
+
+function mouseOverTer() {
+
+  document.getElementById("terceiroParagrafo").style.textShadow = "1px 2px 5px #C0C0C0		";
+
+}
+function mouseOutTer() {
+
+  document.getElementById("terceiroParagrafo").style.textShadow = "0px 0px 0px #0000";
+}
+
+//  --------------------------------- Terceiro Paragrafo ------------------------------ //
+
+
+
+
+
+//  --------------------------------- Quarto Paragrafo ------------------------------ //
+
+function mouseOverQua() {
+
+  document.getElementById("quartoParagrafo").style.textShadow = "1px 2px 5px #C0C0C0		";
+
+}
+function mouseOutQua() {
+
+  document.getElementById("quartoParagrafo").style.textShadow = "0px 0px 0px #0000";
+}
